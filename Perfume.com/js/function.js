d590b93d882c32553dda6c387844c2472722f9e0
ShahzaibButt <shahzaibbutt84@gmail.com>
2019-09-28 21:58:29 +0500

    Html
    A Static Front End Website Based on Perfumes Selling

---
+++ Perfume.com/js/function.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+function google_search()
+{
+	var search_value=document.getElementById("search_box").value;
+	window.open("https://www.google.com.pk/?gws_rd=cr&ei=YRh6U-KSHMWByQP99IDoBw#q="+search_value);
+}
